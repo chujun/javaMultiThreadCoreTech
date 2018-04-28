610b1717b5a06586402151bf9008924ae7e72935
za-chujun <1571970439@qq.com>
2018-04-28 12:00:06 +0800

    temp

--- first/src/test/java/com/jun/chu/demo/createThread/chap1t2/My2ThreadTest.java
+++ first/src/test/java/com/jun/chu/demo/createThread/chap1t2/My2ThreadTest.java
@@ -8,8 +8,30 @@ import org.junit.Test;
  */
 public class My2ThreadTest {
     @Test
-    public void test_Thread_currentThread_method() {
+    public void test_Thread_currentThread_method_start() {
         My2Thread my2Thread = new My2Thread("A");
         my2Thread.start();
+        //        构造方法的打印:Thread.currentThread().getName() = main
+        //        由A计算.count=4
+        //        由A计算.count=3
+        //        由A计算.count=2
+        //        由A计算.count=1
+        //        由A计算.count=0
+        //        run方法的打印:Thread.currentThread().getName() = A
+
+    }
+
+    @Test
+    public void test_Thread_currentThread_method_run() {
+        My2Thread my2Thread = new My2Thread("A");
+        my2Thread.run();
+        //        构造方法的打印:Thread.currentThread().getName() = main
+        //        由main计算.count=4
+        //        由main计算.count=3
+        //        由main计算.count=2
+        //        由main计算.count=1
+        //        由main计算.count=0
+        //        run方法的打印:Thread.currentThread().getName() = main
+
     }
 }
